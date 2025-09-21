b63febbe9c720cbd7dcf98be10644ef42cb4a81b
smallslope <mayaosaka@outlook.com>
2025-09-21 11:30:12 +0100

    feat(color-swatch): Created color-swatch web component
    - Created the color-swatch component. So far it displays the color name, value and a circle containing the color (the actual swatch).
    - The colour can be changed dynamically through the html file.
    - Started working on storing the color-swatch data in an array to populate the information more easily and to work towards creating a color-swatches component that will include multiple color-swatch components.

---
+++ script.js
@@ -0,0 +1,22 @@
+var swatchList = [
+{
+    swatch_color: "",
+    swatch_name: "",
+    swatch_color_label: ""
+},
+{
+    swatch_color: "",
+    swatch_name: "",
+    swatch_color_label: ""
+},
+{
+    swatch_color: "",
+    swatch_name: "",
+    swatch_color_label: ""
+},
+{
+    swatch_color: "",
+    swatch_name: "",
+    swatch_color_label: ""
+},
+]
