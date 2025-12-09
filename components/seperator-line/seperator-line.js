f07dc73cbc047022a59c041c7363a4966576f0cb
smallslope <mayaosaka@outlook.com>
2025-12-09 18:27:31 +0000

    feat(text-block + seperator-line): Set up files
    Set up the base functionality for the text-block and seperator-line web components. This involved setting up the basic functionality for each file.

--- components/seperator-line/seperator-line.js
+++ components/seperator-line/seperator-line.js
@@ -1,16 +1,16 @@
-class TextBlock extends HTMLElement {
+class SeperatorLine extends HTMLElement {
     constructor(){
         super()
         this.attachShadow({mode: "open"})
     }
     async connectedCallback(){
-        const res = await fetch("./components/text-block/text-block.html");
+        const res = await fetch("./components/seperator-line/seperator-line.html");
         const html = await res.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, "text/html");
-        const template = doc.getElementById("text-block-template");
+        const template = doc.getElementById("seperator-line-template");
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         console.log(template);
     }
 }
-customElements.define("text-block", TextBlock);
+customElements.define("seperator-line", SeperatorLine);
