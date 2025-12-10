01d20470b5482911b1333274af8f2a3b947a51aa
smallslope <mayaosaka@outlook.com>
2025-12-10 18:37:20 +0000

    feat: Refactor component styling to properly use ::slotted and ::host
    Updated CSS to apply styles within each component’s shadow DOM instead of targeting slotted elements from the light DOM, resolving previous styling conflicts. Removed unnecessary <span> wrappers around <img> and <video> elements after correcting my understanding of how slotted content works.

--- pages/sensco-app-page/sensco-app-page.js
+++ pages/sensco-app-page/sensco-app-page.js
@@ -1,16 +1,16 @@
-class ProjectPage extends HTMLElement {
+class SenscoAppPage extends HTMLElement {
     constructor(){
         super()
         this.attachShadow({mode: "open"})
     }
     async connectedCallback(){
-        const res = await fetch("./components/project-page/project-page.html");
+        const res = await fetch("./pages/sensco-app-page/sensco-app-page.html");
         const html = await res.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, "text/html");
-        const template = doc.getElementById("project-page-template");
+        const template = doc.getElementById("sensco-app-page-template");
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         console.log(template);
     }
 }
-customElements.define("project-page", ProjectPage);
+customElements.define("sensco-app-page", SenscoAppPage);
