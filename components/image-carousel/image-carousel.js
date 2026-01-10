e5502c26fb5c7ecb36c7e24b02fd663e314d946e
smallslope <mayaosaka@outlook.com>
2026-01-10 21:58:45 +0000

    feat(image-carousel): Created component

--- components/image-carousel/image-carousel.js
+++ components/image-carousel/image-carousel.js
@@ -10,7 +10,60 @@ class ImageCarousel extends HTMLElement {
         const doc = parser.parseFromString(html, "text/html");
         const template = doc.getElementById("image-carousel-template");
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        console.log(template);
+
+        this.currentIndex = 0;
+        this.slidesEl = this.shadowRoot.querySelector('.slides');
+        this.dotsEl = this.shadowRoot.querySelector('.dots');
+        this.slotEl = this.shadowRoot.querySelector('slot');
+
+        this.prevArrow = this.shadowRoot.querySelector('.arrow-left');
+        this.nextArrow = this.shadowRoot.querySelector('.arrow-right');
+
+        this.slotEl.addEventListener('slotchange', () => {
+            this.slides = this.slotEl.assignedElements();
+            this.updateDots();
+            this.updateSlide();
+            console.log(this.slides.length);
+          });
+
+        this.prevArrow.addEventListener('click', () => this.showPrev());
+        this.nextArrow.addEventListener('click', () => this.showNext());
     }
+    updateDots() {
+        this.dotsEl.innerHTML = '';
+        this.slides.forEach((_, index) => {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        if (index === this.currentIndex) dot.classList.add('active');
+        this.dotsEl.appendChild(dot);
+        });
+     } 
+     updateSlide() {
+        this.slides.forEach((slide, i) => {
+          slide.style.display = i === this.currentIndex ? 'block' : 'none';
+        });
+    
+        // Update dots
+        [...this.dotsEl.children].forEach((dot, i) => {
+          dot.classList.toggle('active', i === this.currentIndex);
+        });
+      }
+    showPrev() {
+        if (this.currentIndex > 0) {
+          this.currentIndex--;
+        } else {
+          this.currentIndex = this.slides.length - 1;
+        }
+        this.updateSlide();
+    }
+    showNext() {
+        if (this.currentIndex < this.slides.length - 1) {
+          this.currentIndex++;
+        } else {
+          this.currentIndex = 0;
+        }
+        this.updateSlide();
+    }
+
 }
 customElements.define("image-carousel", ImageCarousel);
