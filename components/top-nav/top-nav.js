b44fdd4c43fadb3371545eaa1e1270b44e6e53f0
smallslope <mayaosaka@outlook.com>
2026-02-19 19:26:01 +0000

    feat(sensco-app-page): Uploaded content for the Sensco App page

--- components/top-nav/top-nav.js
+++ components/top-nav/top-nav.js
@@ -18,7 +18,7 @@ class TopNav extends HTMLElement {
                 links.forEach(link => link.classList.remove('active'));
             }
         });
-    }
+    } 
     //Each link is linked to the appropriate data attribute (data-page)
     setupNav(){
         const links = this.shadowRoot.querySelectorAll('.top-nav__link, .top-nav__title');
@@ -29,19 +29,25 @@ class TopNav extends HTMLElement {
     }
     setActiveLink(clickedLink, page){
         const links = this.shadowRoot.querySelectorAll('.top-nav__link');
-        if(clickedLink.classList.contains('top-nav__link')){
-            links.forEach(link => link.classList.remove('active'));
-            if (clickedLink.dataset.page !== page) return;
-            clickedLink.classList.add('active');
-        }
-        else if (clickedLink.classList.contains('top-nav__title')) {
-            const homeLink = this.shadowRoot.querySelector('.top-nav__link[data-page="homePage"]');
-            console.log('homeLink Active')
-            if (!homeLink) return;
-            links.forEach(link => link.classList.remove('active'));
-            homeLink.classList.add('active');
+        if(window.innerWidth < 768){
 
         }
+        else{
+            if(clickedLink.classList.contains('top-nav__link')){
+                links.forEach(link => link.classList.remove('active'));
+                if (clickedLink.dataset.page !== page) return;
+                clickedLink.classList.add('active');
+            }
+            else if (clickedLink.classList.contains('top-nav__title')) {
+                const homeLink = this.shadowRoot.querySelector('.top-nav__link[data-page="homePage"]');
+                console.log('homeLink Active')
+                if (!homeLink) return;
+                links.forEach(link => link.classList.remove('active'));
+                homeLink.classList.add('active');
+    
+            }
+        }
+        
     }
     //When a link is clicked it triggers the dispathOpenPage function
     openPage(navLink, page){
@@ -62,72 +68,7 @@ class TopNav extends HTMLElement {
                     page
                 }
             }));
-    } 
+    }
 }
 customElements.define("top-nav", TopNav);
-        // const menuIcon = this.shadowRoot.querySelector("menu-icon");
-        // const menu = this.shadowRoot.querySelector(".top-nav__menu");
-        // const links = this.shadowRoot.querySelectorAll(".top-nav__link");
-
     
-        // //If a top nav link is clicked it removed the active status from the existing link and adds it to the one that has been clicked. 
-        // links.forEach(link => {
-        //     link.addEventListener("click", () => {
-        //         links.forEach(l => l.classList.remove("top-nav__link--active"));
-        //         link.classList.add("top-nav__link--active");
-        //         //If the display is smaller than 768px 
-        //         if (window.innerWidth <= 768){
-        //             menuIcon.removeAttribute("open");
-        //             menu.style.display = "none";
-        //             window.dispatchEvent(new CustomEvent("resizetoggle", { detail: {}}));
-        //         }
-        //     });
-        // });
-        
-        // const title = this.shadowRoot.querySelector(".top-nav__title");
-        // const homeLink = this.shadowRoot.getElementById("home-link");
-
-
-        
-
-        // if (title){
-        //     title.addEventListener("click", () => {
-        //         links.forEach( l => l.classList.remove("top-nav__link--active"));
-
-        //         if (homeLink){
-        //             homeLink.classList.add("top-nav__link--active");
-        //             eventDispatcher(title, )
-        //         };
-               
-
-        //         if (window.innerWidth <= 768){
-        //             menuIcon.removeAttribute("open");
-        //             menu.style.display = "none";
-        //         }
-        //     });
-        // }
-        // menuIcon.addEventListener("toggle", (e) => {
-        //     const isOpen = e.detail.open;
-
-        //     if (window.innerWidth <= 768) {
-        //         menu.style.display = isOpen ? "flex" : "none";
-        //     }
-        // });
-        // const handleResize = () => {
-        //     if (window.innerWidth > 768){
-        //         menu.style.display = "flex";
-        //         menuIcon.style.display = "none";
-        //     } else{
-        //         menu.style.display = "none";
-        //         menuIcon.style.display = "block";
-        //         menuIcon.removeAttribute("open");
-        //     }
-        // };
-        // handleResize();
-        // window.addEventListener("resize", handleResize);
-
-    // pageClicked(event){
-    //     console.log(event.target.dataset.page);   
-    // }
-
-
